bba84c6f1a6e5d661de3c42802ee220c633f13c1
Matt <semisentientfungus@gmail.com>
2022-06-14 13:08:19 +0200

    add basic game functionality

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
+//code for the actual game
+let playerSelection = prompt('Rock! Paper! Scissors!');
+let computerSelection = computerPlay();
+game(playerSelection, computerSelection);
+
+
+//functions
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3) +1;
     switch(randomNumber){
@@ -13,5 +20,44 @@ function computerPlay() {
     }
 }
 
-let computer = computerPlay();
-console.log(computer);
+function game(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection == "rock"){
+        if (computerSelection == "Rock"){
+            console.log("Draw!");
+        }
+        else if (computerSelection == "Paper"){
+            console.log("You lose! Paper beats rock!");
+        }
+        else {
+            console.log("You win! Rock beats scissors!");
+        }
+    }
+    else if (playerSelection == "paper"){
+        if (computerSelection == "Rock"){
+            console.log("You win! Paper beats rock!");
+        }
+        else if (computerSelection == "Paper"){
+            console.log("Draw!");
+        }
+        else {
+            console.log("You lose! Scissors beat paper!");
+        }
+    }
+    else if (playerSelection == "scissors") {
+        if (computerSelection == "Rock"){
+            console.log("You lose! Rock beats scissors!");
+        }
+        else if (computerSelection == "Paper"){
+            console.log("You win! Scissors beat paper!");
+        }
+        else {
+            console.log("Draw!");
+        }
+    }
+    else {
+        console.log("Ummm, are you sure you logged the right option?");
+    }
+}
